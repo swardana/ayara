1920ba3d852f1655a6df943f8e174b027058974c
Sukma Wardana <swardana@tutanota.com>
2021-07-23 13:26:06 +0700

    Add nhentai.net constants

---
+++ src/main/java/com/swardana/ayara/manga/nhentai/Nhentai.java
@@ -0,0 +1,53 @@
+/*
+ * Ayara, non-interactive nhentai.net manga downloader
+ * Copyright (C) 2021  Sukma Wardana
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.swardana.ayara.manga.nhentai;
+
+/**
+ * A nhentai.net constants.
+ *
+ * @author Sukma Wardana
+ */
+public final class Nhentai {
+
+    /**
+     * Hide the constructor.
+     */
+    private Nhentai() {}
+
+    /**
+     * The nhentai.net name.
+     */
+    public static final String NAME = "nhentai.net";
+
+    /**
+     * The nhentai.net website url.
+     */
+    public static final String URL = "https://nhentai.net/";
+
+    /**
+     * The nhentai.net website hentai manga url.
+     */
+    public static final String WEBSITE_URL = "https://nhentai.net/g/";
+
+    /**
+     * The nhentai.net website hentai manga gallery url.
+     */
+    public static final String GALLERY_URL = "https://i.nhentai.net/galleries/";
+
+}
