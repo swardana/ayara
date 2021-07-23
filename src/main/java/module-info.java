4e50848a26d0ccde2efad3eaaca1e07ccb309980
Sukma Wardana <swardana@tutanota.com>
2021-07-23 11:30:03 +0700

    Add JPMS module descriptor
    Made the Ayara application as modular Java project.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,33 @@
+/*
+ * Ayara, non-interactive nhentai.net manga downloader
+ * Copyright (C) 2021  Sukma Wardana
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/**
+ * The ayara module descriptor.
+ *
+ * @moduleGraph
+ */
+module com.swardana.ayara {
+
+    requires java.base;
+    requires java.net.http;
+
+    requires org.jsoup;
+
+    exports com.swardana.ayara;
+
+}
