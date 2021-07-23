a6dc23effff4625f894ab9dae0d29bc2d9b52055
Sukma Wardana <swardana@tutanota.com>
2021-07-23 16:01:51 +0700

    Add Progress API contract

---
+++ src/main/java/com/swardana/ayara/message/Progress.java
@@ -0,0 +1,36 @@
+/*
+ * Ayara, non-interactive nhentai.net manga downloader
+ * Copyright (C) 2021  Sukma Wardana
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.swardana.ayara.message;
+
+/**
+ * A progress bar.
+ *
+ * @author Sukma Wardana
+ */
+public interface Progress {
+
+    /**
+     * Print this progress bar.
+     *
+     * @param current the current task being executed.
+     * @throws IllegalArgumentException if the current equal to zero (0).
+     */
+    void print(long current);
+
+}
