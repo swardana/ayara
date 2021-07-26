f9fc4c486d76b74b232e40c42ce7642aee521079
Sukma Wardana <swardana@tutanota.com>
2021-07-26 16:09:28 +0700

    Add Ayara application process

--- src/main/java/com/swardana/ayara/AyaraConsole.java
+++ src/main/java/com/swardana/ayara/AyaraConsole.java
@@ -18,6 +18,22 @@
 
 package com.swardana.ayara;
 
+import com.swardana.ayara.download.ChannelDownload;
+import com.swardana.ayara.download.MkdirDownload;
+import com.swardana.ayara.download.NoDuplicateDownload;
+import com.swardana.ayara.download.PageDownload;
+import com.swardana.ayara.manga.nhentai.NhentaiExistence;
+import com.swardana.ayara.manga.nhentai.NhentaiPages;
+import com.swardana.ayara.message.ConsoleHelp;
+import com.swardana.ayara.message.ConsoleMessage;
+import com.swardana.ayara.message.ConsoleProgress;
+import com.swardana.ayara.message.ConsoleVersion;
+import com.swardana.ayara.message.Help;
+import com.swardana.ayara.message.Message;
+import com.swardana.ayara.message.Version;
+
+import java.nio.file.Path;
+
 /**
  * The bootstrap ayara command-line interface.
  *
@@ -25,13 +41,122 @@ package com.swardana.ayara;
  */
 public class AyaraConsole {
 
+    private static final Version VERSION = new ConsoleVersion();
+    private static final Message MESSAGE = new ConsoleMessage();
+    private static final Help HELP = new ConsoleHelp();
+
+    private static String outdir;
+    private static String code;
+    private static boolean isContinue;
+    private static boolean isForce;
+
     /**
      * The ayara-cli main method.
      *
      * @param args the ayara-cli arguments.
      */
     public static void main(final String[] args) {
+        if (args.length == 0) {
+            MESSAGE.print("Missing hentai digit code!", 1);
+        }
+        int idx = 0;
+        while (idx < args.length) {
+            var arg = args[idx];
+            switch (arg) {
+                case "-h":
+                case "--help":
+                    HELP.print();
+                    break;
+                case "-V":
+                case "--version":
+                    VERSION.print();
+                    break;
+                case "-c":
+                case "--continue":
+                    isContinue = true;
+                    break;
+                case "-O":
+                case "--output-directory":
+                    outdir = args[++idx];
+                    break;
+                case "-x":
+                case "--force-directories":
+                    isForce = true;
+                    break;
+                default:
+                    if (arg.startsWith("-")) {
+                        MESSAGE.print(
+                            "Unknown option " + arg,
+                            1
+                        );
+                    } else {
+                        code = arg;
+                    }
+                    break;
+            }
+            idx++;
+        }
+        process();
+    }
 
+    private static void process() {
+        if (code == null) {
+            MESSAGE.print("Missing hentai manga digit code!", 1);
+        }
+        var existence = new NhentaiExistence(code);
+        try {
+            if (!existence.isExist()) {
+                MESSAGE.print(
+                    "The hentai manga is not found, please try different digit code!",
+                    1
+                );
+            }
+            var target = pathDirectory();
+            var pages = new NhentaiPages(code);
+            var command = pageDownload(target);
+            var content = """
+                Processing to download hentai manga:
+                Code          : %s
+                Save to       : %s
+                """.formatted(code, target.toString());
+            System.out.println(content);
+
+            long total = pages.size();
+            long start = System.currentTimeMillis();
+            var progress = new ConsoleProgress(start, total);
+            int current = 1;
+            for (final var page : pages) {
+                command.download(page);
+                progress.print(current++);
+            }
+
+        } catch (final Exception ex) {
+            MESSAGE.print(
+                "Something went wrong!" + ex.getMessage(),
+                1
+            );
+        }
+    }
+
+    private static Path pathDirectory() {
+        final Path target;
+        if (outdir == null) {
+            target = Path.of("").toAbsolutePath();
+        } else {
+            target = Path.of(outdir);
+        }
+        return target;
+    }
+
+    private static PageDownload pageDownload(final Path target) {
+        PageDownload command = new ChannelDownload(target);
+        if (isForce) {
+            command = new MkdirDownload(command, target);
+        }
+        if (isContinue) {
+            command = new NoDuplicateDownload(command, target);
+        }
+        return command;
     }
 
 }
